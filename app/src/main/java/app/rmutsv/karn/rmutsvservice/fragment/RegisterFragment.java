2ddd56402bb68702c7a13a578bf3f97e31d54dc1
Karn <karn52057@gmail.com>
2017-11-07 12:02:40 +0700

    before Lunch

---
+++ app/src/main/java/app/rmutsv/karn/rmutsvservice/fragment/RegisterFragment.java
@@ -0,0 +1,23 @@
+package app.rmutsv.karn.rmutsvservice.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.rmutsv.karn.rmutsvservice.R;
+
+/**
+ * Created by lenovo on 7/11/2560.
+ */
+
+public class RegisterFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container,false);
+        return view;
+    }
+}   // Main Class
