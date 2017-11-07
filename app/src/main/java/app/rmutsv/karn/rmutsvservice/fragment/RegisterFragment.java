5b120028c2b1fdfb7938a7a82f4c70d9bd0ae18c
Karn <karn52057@gmail.com>
2017-11-07 16:36:17 +0700

    Finish 7nov

--- app/src/main/java/app/rmutsv/karn/rmutsvservice/fragment/RegisterFragment.java
+++ app/src/main/java/app/rmutsv/karn/rmutsvservice/fragment/RegisterFragment.java
@@ -3,17 +3,125 @@ package app.rmutsv.karn.rmutsvservice.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 
+
+import app.rmutsv.karn.rmutsvservice.MainActivity;
 import app.rmutsv.karn.rmutsvservice.R;
+import app.rmutsv.karn.rmutsvservice.utility.MyAlert;
 
 /**
  * Created by lenovo on 7/11/2560.
  */
 
 public class RegisterFragment extends Fragment{
+ //     Explicit
+    private String nameString, userString, passwordString, categoryString;
+    private boolean aBoolean = true;
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Toolbar Controller
+        toolbarController();
+
+//        Save Controller
+        saveController();
+
+//        Category Controller
+        categoryController();
+
+
+    }   // Main Method
+
+    private void categoryController() {
+        RadioGroup radioGroup = getView().findViewById(R.id.ragCategory);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                aBoolean = false;
+
+                switch (i) {
+                    case R.id.radBuyer:
+                        categoryString = "Buyer";
+                        break;
+                    case R.id.radSaleser:
+                        categoryString = "Saler";
+                        break;
+                }
+
+            }
+        });
+    }
+
+    private void saveController() {
+        ImageView imageView = getView().findViewById(R.id.imvSave);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Initial View
+                EditText nameEditText = getView().findViewById(R.id.edtName);
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordText = getView().findViewById(R.id.edtPassword);
+
+//                Change Data Type
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordText.getText().toString().trim();
+
+//                Check Space
+                if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+//                    Have Space
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog("Have Space",
+                            "Please Fill All Every Blank");
+                } else if (aBoolean) {
+//                    Non Choose Choice
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog("Non Chose Category", "Please Chose Cateory");
+
+
+                } else {
+//                    Choosed Choice
+
+                }
+
+
+            } // onClick
+        });
+    }
+
+    private void toolbarController() {
+
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity())
+                .getSupportActionBar()
+                .setTitle(getResources().getString(R.string.register));
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
---
+++ app/src/main/java/app/rmutsv/karn/rmutsvservice/utility/MyAlert.java
@@ -0,0 +1,41 @@
+package app.rmutsv.karn.rmutsvservice.utility;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+import app.rmutsv.karn.rmutsvservice.R;
+
+/**
+ * Created by lenovo on 7/11/2560.
+ */
+
+public class MyAlert {
+
+    //    Explicit
+    private Context context;
+
+    public MyAlert(Context context) {
+        this.context = context;
+    }
+
+    public void myDialog(String strTitle, String strMessage) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.ic_action_alert);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+
+
+}   // Main Class
