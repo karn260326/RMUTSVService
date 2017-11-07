eb39f19ffe46d949cc86350ad04d7b5356d634ea
Karn <karn52057@gmail.com>
2017-11-07 10:35:04 +0700

    Main Fragment success

--- app/src/main/java/app/rmutsv/karn/rmutsvservice/MainActivity.java
+++ app/src/main/java/app/rmutsv/karn/rmutsvservice/MainActivity.java
@@ -3,11 +3,25 @@ package app.rmutsv.karn.rmutsvservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.rmutsv.karn.rmutsvservice.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//       Add Fragment to Activity
+        if (savedInstanceState == null) {
+
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment()).commit();
+
+        }
+
+
+    }   // Main Method
+
+}   // Main Class
