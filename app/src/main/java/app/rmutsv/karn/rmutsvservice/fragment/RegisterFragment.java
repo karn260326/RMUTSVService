fd40fcab723728d7426372080eedf9e7674dfa75
Karn <karn52057@gmail.com>
2017-11-08 11:39:59 +0700

    Post data ok

--- app/src/main/java/app/rmutsv/karn/rmutsvservice/fragment/RegisterFragment.java
+++ app/src/main/java/app/rmutsv/karn/rmutsvservice/fragment/RegisterFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,8 @@ import android.widget.RadioGroup;
 import app.rmutsv.karn.rmutsvservice.MainActivity;
 import app.rmutsv.karn.rmutsvservice.R;
 import app.rmutsv.karn.rmutsvservice.utility.MyAlert;
+import app.rmutsv.karn.rmutsvservice.utility.Myconstant;
+import app.rmutsv.karn.rmutsvservice.utility.UploadNewUser;
 
 /**
  * Created by lenovo on 7/11/2560.
@@ -93,12 +96,30 @@ public class RegisterFragment extends Fragment{
 
                 } else {
 //                    Choosed Choice
-
+                    uploadUserToServer();
                 }
 
 
             } // onClick
         });
+    }
+
+    private void uploadUserToServer() {
+
+        String tag = "8novV1";
+        try {
+
+            Myconstant myconstant = new Myconstant();
+            UploadNewUser uploadNewUser = new UploadNewUser(getActivity());
+            uploadNewUser.execute(nameString, categoryString,
+                    userString, passwordString, myconstant.getUrlPostData());
+            String result = uploadNewUser.get();
+            Log.d(tag,"Result" + e.toString())
+
+        }
+
+        }catch (Exception e){
+            Log.d(tag, "e ===>" + e.toString());
     }
 
     private void toolbarController() {
---
+++ app/src/main/java/app/rmutsv/karn/rmutsvservice/utility/Myconstant.java
@@ -0,0 +1,14 @@
+package app.rmutsv.karn.rmutsvservice.utility;
+
+/**
+ * Created by lenovo on 8/11/2560.
+ */
+
+public class Myconstant {
+
+    private String urlPostData = "http://androidthai.in.th/rmuts/addDataSaowaluck.php";
+
+    public String getUrlPostData() {
+        return urlPostData;
+    }
+}   // Main Class
